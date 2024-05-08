ed281c900c9f4b00ea4f7b53e66ecdfed0010f51
dimichai <9805570+dimichai@users.noreply.github.com>
2024-05-08 18:52:21 +0200

    adapted gpi_pd to the tndp version

--- morl_baselines/common/evaluation.py
+++ morl_baselines/common/evaluation.py
@@ -11,6 +11,7 @@ def eval_mo(
     w: Optional[np.ndarray] = None,
     scalarization=np.dot,
     render: bool = False,
+    return_obs: bool = False,
 ) -> Tuple[float, float, np.ndarray, np.ndarray]:
     """Evaluates one episode of the agent in the environment.
 
@@ -20,18 +21,21 @@ def eval_mo(
         scalarization: scalarization function, taking weights and reward as parameters
         w (np.ndarray): Weight vector
         render (bool, optional): Whether to render the environment. Defaults to False.
+        return_obs (bool, optional): Whether to return a list of the observations seen during the episode. Defaults to False.
 
     Returns:
         (float, float, np.ndarray, np.ndarray): Scalarized return, scalarized discounted return, vectorized return, vectorized discounted return
     """
-    obs, _ = env.reset()
+    obs, info = env.reset()
+    all_obs = [obs[0]]
     done = False
     vec_return, disc_vec_return = np.zeros_like(w), np.zeros_like(w)
     gamma = 1.0
     while not done:
         if render:
             env.render()
-        obs, r, terminated, truncated, info = env.step(agent.eval(obs, w))
+        obs, r, terminated, truncated, info = env.step(agent.eval(obs, w, action_mask=info['action_mask']))
+        all_obs.append(obs[0])
         done = terminated or truncated
         vec_return += r
         disc_vec_return += gamma * r
@@ -44,12 +48,21 @@ def eval_mo(
         scalarized_return = scalarization(w, vec_return)
         scalarized_discounted_return = scalarization(w, disc_vec_return)
 
-    return (
-        scalarized_return,
-        scalarized_discounted_return,
-        vec_return,
-        disc_vec_return,
-    )
+    if return_obs:
+        return (
+            scalarized_return,
+            scalarized_discounted_return,
+            vec_return,
+            disc_vec_return,
+            all_obs
+        )
+    else:
+        return (
+            scalarized_return,
+            scalarized_discounted_return,
+            vec_return,
+            disc_vec_return,
+        )
 
 
 def eval_mo_reward_conditioned(
@@ -98,7 +111,7 @@ def eval_mo_reward_conditioned(
     )
 
 
-def policy_evaluation_mo(agent, env, w: np.ndarray, rep: int = 5) -> Tuple[float, float, np.ndarray, np.ndarray]:
+def policy_evaluation_mo(agent, env, w: np.ndarray, rep: int = 5, return_obs: bool = False) -> Tuple[float, float, np.ndarray, np.ndarray]:
     """Evaluates the value of a policy by running the policy for multiple episodes. Returns the average returns.
 
     Args:
@@ -106,19 +119,32 @@ def policy_evaluation_mo(agent, env, w: np.ndarray, rep: int = 5) -> Tuple[float
         env: MO-Gymnasium environment
         w (np.ndarray): Weight vector
         rep (int, optional): Number of episodes for averaging. Defaults to 5.
+        return_obs (bool, optional): Whether to return a list of the observations seen during the episode. Defaults to False.
 
     Returns:
         (float, float, np.ndarray, np.ndarray): Avg scalarized return, Avg scalarized discounted return, Avg vectorized return, Avg vectorized discounted return
     """
-    evals = [eval_mo(agent, env, w) for _ in range(rep)]
+    evals = [eval_mo(agent, env, w, return_obs=True) for _ in range(rep)]
+    
     avg_scalarized_return = np.mean([eval[0] for eval in evals])
     avg_scalarized_discounted_return = np.mean([eval[1] for eval in evals])
     avg_vec_return = np.mean([eval[2] for eval in evals], axis=0)
     avg_disc_vec_return = np.mean([eval[3] for eval in evals], axis=0)
+    obs = [eval[4] for eval in evals]
 
+
+    if return_obs:
+        return (
+            avg_scalarized_return,
+            avg_scalarized_discounted_return,
+            avg_vec_return,
+            avg_disc_vec_return,
+            obs
+        )
+        
     return (
         avg_scalarized_return,
         avg_scalarized_discounted_return,
         avg_vec_return,
-        avg_disc_vec_return,
+        avg_disc_vec_return
     )
