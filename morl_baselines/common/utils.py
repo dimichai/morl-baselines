592c823f81fc7405f5df16e56db7107f87d470d9
dimichai <9805570+dimichai@users.noreply.github.com>
2024-06-24 14:47:31 +0200

    evaluation.py and utils.py - sync with upstream

--- morl_baselines/common/utils.py
+++ morl_baselines/common/utils.py
@@ -6,15 +6,14 @@ from typing import Callable, List
 import numpy as np
 
 
-class MOSyncVectorEnv(SyncVectorEnv, EzPickle):
-    """Vectorized environment that serially runs multiple environments."""
+def linearly_decaying_value(initial_value, decay_period, step, warmup_steps, final_value):
+    """Returns the current value for a linearly decaying parameter.
 
-    def __init__(
-        self,
-        env_fns: Iterator[callable],
-        copy: bool = True,
-    ):
-        """Vectorized environment that serially runs multiple environments.
+    This follows the Nature DQN schedule of a linearly decaying epsilon (Mnih et
+    al., 2015). The schedule is as follows:
+    Begin at 1. until warmup_steps steps have been taken; then
+    Linearly decay epsilon from 1. to epsilon in decay_period steps; and then
+    Use epsilon from there on.
 
     Args:
         decay_period: float, the period over which the value is decayed.
